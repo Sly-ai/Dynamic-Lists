1946ca576c23796dbddd05b222ce034b743f35ac
Sly-ai <85676151+Sly-ai@users.noreply.github.com>
2024-01-18 11:51:53 +0000

    Added to text to line 11, included upper and lower case consideration for line 12,18,21

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os, time
+
 title = "\033[31m=\033[0m=\033[34m=\033[33m To Do List Manager \033[31m=\033[0m=\033[34m="
 
 print(f"{title:^80}")
@@ -6,14 +8,17 @@ print()
 ToDoList = []
 
 def printList():
-  menu = input("ToDoList Manager\n\nDo you want to view, add or edit the todo list?\n:> ")
-  if menu == "view":
+  menu = input("ToDoList Manager\n\n What do you want to do?\n view\n add\n remove\n\n:> ")
+  if menu == "view" or menu == "View" or menu == "VIEW":
     for item in ToDoList:
-      print(item)
-  elif menu == "add":
+      counter = 0
+      counter += 1
+      print(f"{counter}:{item}")
+      time.sleep(5)
+  elif menu == "add" or menu == "Add" or menu == "ADD":
     item = input("What's next on the Agenda?: ")
     ToDoList.append(item)
-  elif menu == "remove":
+  elif menu == "remove" or menu == "Remove" or menu == "REMOVE":
     item = ("What do you want to remove?: ")
     if item in ToDoList:
       ToDoList.remove(item)
@@ -23,4 +28,5 @@ def printList():
 while True:
   printList()
   print()
+  os.system("clear")
 
